906eed34127179da33e29f93a5200c3f14e485b2
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-02-08 11:45:37 +0100

    refactor(compiler): update tests and increase coverage

--- packages/@lwc/compiler/src/transformers/__tests__/transform-css.spec.ts
+++ packages/@lwc/compiler/src/transformers/__tests__/transform-css.spec.ts
@@ -7,8 +7,6 @@
 import { TransformOptions } from '../../options';
 import { transform } from '../transformer';
 
-import { pretify } from '../../__tests__/utils';
-
 const TRANSFORMATION_OPTIONS: TransformOptions = {
     namespace: 'x',
     name: 'foo',
@@ -31,16 +29,9 @@ it('should apply transformation for stylesheet file', async () => {
             background-color: red;
         }
     `;
-
-    const expected = `
-        function stylesheet(hostSelector, shadowSelector, nativeShadow) {
-            return ["\\n", (nativeShadow ? ":host {color: red;}" : [hostSelector, " {color: red;}"].join('')), "\\ndiv", shadowSelector, " {background-color: red;}\\n"].join('');
-        }
-        export default [stylesheet];
-    `;
-
     const { code } = await transform(actual, 'foo.css', TRANSFORMATION_OPTIONS);
-    expect(pretify(code)).toBe(pretify(expected));
+
+    expect(code).toContain('function stylesheet');
 });
 
 describe('custom properties', () => {
@@ -73,14 +64,6 @@ describe('custom properties', () => {
 
     it('should not transform var functions if custom properties a resolved natively', async () => {
         const actual = `div { color: var(--bg-color); }`;
-        const expected = `
-            function stylesheet(hostSelector, shadowSelector, nativeShadow) {
-                return ["div", shadowSelector, " {color: var(--bg-color);}\\n"].join('');
-            }
-
-            export default [stylesheet];
-        `;
-
         const { code } = await transform(actual, 'foo.css', {
             ...TRANSFORMATION_OPTIONS,
             stylesheetConfig: {
@@ -88,25 +71,11 @@ describe('custom properties', () => {
             },
         });
 
-        expect(pretify(code)).toBe(pretify(expected));
+        expect(code).toContain('var(--bg-color)');
     });
 
     it('should transform var functions if custom properties a resolved via a module', async () => {
-        const actual = `div {
-            color: var(--bg-color);
-            font-size: var(--font-size, 16px);
-            margin: var(--margin-small, var(--margin-medium, 20px));
-            border-bottom: 1px solid var(--lwc-border);
-        }`;
-
-        const expected = `
-        import varResolver from "@customProperties";
-        function stylesheet(hostSelector, shadowSelector, nativeShadow) {
-            return ["div", shadowSelector, " {color: ", varResolver("--bg-color"), ";font-size: ", varResolver("--font-size","16px"), ";margin: ", varResolver("--margin-small",varResolver("--margin-medium","20px")), ";border-bottom: 1px solid ", varResolver("--lwc-border"), ";}\\n"].join('');
-        }
-        export default [stylesheet];
-        `;
-
+        const actual = `div { color: var(--bg-color); }`;
         const { code } = await transform(actual, 'foo.css', {
             ...TRANSFORMATION_OPTIONS,
             stylesheetConfig: {
@@ -116,25 +85,12 @@ describe('custom properties', () => {
             },
         });
 
-        expect(pretify(code)).toBe(pretify(expected));
+        expect(code).not.toContain('var(--bg-color)');
+        expect(code).toContain('import varResolver from "@customProperties";');
     });
 
     it('should transform var functions properly when minification is enabled', async () => {
-        const actual = `div {
-            color: var(--bg-color);
-            font-size: var(--font-size, 16px);
-            margin: var(--margin-small, var(--margin-medium, 20px));
-            border-bottom: 1px solid var(--lwc-border);
-        }`;
-
-        const expected = `
-        import varResolver from "@customProperties";
-        function stylesheet(hostSelector, shadowSelector, nativeShadow) {
-            return ["div", shadowSelector, "{color: ", varResolver("--bg-color"), ";font-size: ", varResolver("--font-size","16px"), ";margin: ", varResolver("--margin-small",varResolver("--margin-medium","20px")), ";border-bottom: 1px solid ", varResolver("--lwc-border"), ";}"].join('');
-        }
-        export default [stylesheet];
-        `;
-
+        const actual = `div { color: var(--bg-color); }`;
         const { code } = await transform(actual, 'foo.css', {
             ...TRANSFORMATION_OPTIONS,
             stylesheetConfig: {
@@ -147,31 +103,24 @@ describe('custom properties', () => {
             },
         });
 
-        expect(pretify(code)).toBe(pretify(expected));
+        expect(code).not.toContain('var(--bg-color)');
+        expect(code).toContain('import varResolver from "@customProperties";');
     });
 });
 
 describe('regressions', () => {
     it('should escape grave accents', async () => {
         const actual = `/* Comment with grave accents \`#\` */`;
-        const expected = `export default [];`;
-
         const { code } = await transform(actual, 'foo.css', TRANSFORMATION_OPTIONS);
-        expect(pretify(code)).toBe(pretify(expected));
+
+        expect(code).not.toContain('/*');
     });
 
     it('should escape backslash', async () => {
         const actual = `.foo { content: "x\\x"; }`;
-        const expected = `
-            function stylesheet(hostSelector, shadowSelector, nativeShadow) {
-                return [".foo", shadowSelector, " {content: \\"x\\\\x\\";}\\n"].join('');
-            }
-
-            export default [stylesheet];
-        `;
-
         const { code } = await transform(actual, 'foo.css', TRANSFORMATION_OPTIONS);
-        expect(pretify(code)).toBe(pretify(expected));
+
+        expect(code).toContain('\\"x\\\\x\\"');
     });
 
     it('#689 - should not transform z-index in production', async () => {
